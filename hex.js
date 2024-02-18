8d797d0f8b715d627ca41e547be77cfd6b8b9352
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-02-18 03:28:08 +0100

    skapat en funktion som hämtar ett randmom hex färg värde

--- hex.js
+++ hex.js
@@ -1,1 +1,17 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+/*En funktion som lyssnar på klick på knapp och agerar utifrån funtkion*/
+btn.addEventListener("click", function () {
+  /*Anledningen till att man har # är för att hex värdet kommer inte generera en färg utan det,Använder let för att kunna skapa en loop och ge de ett värde, med const så funkar det inte*/
+
+  let hexColor = "#";
+  /*Starta Loop från 0 och den ska loopa tills den når 6 gånger*/
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[0];
+  }
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
