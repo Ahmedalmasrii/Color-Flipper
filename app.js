e74cadbb583d8abcdfeee49e9cf6f4c2cecdb215
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-02-18 03:08:39 +0100

    lagt till en funktion som ändrar färg smat räknar från 0

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
  */
  btn.addEventListener('click', function(){
     /*Hämtar en random nummer mellan 0-3*/
-    const randomNumber = 2;
+    const randomNumber = getRandomNumber();
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
   })
+
+  function getRandomNumber() {
+    return Math.floor(Math.random()* colors.length);
+  }
